1b43181fc10e015fbe1d06a283a30ce2dff0c457
LipehSantos213 <fl9558125@gmail.com>
2026-03-21 11:01:15 -0300

    feat: provider update and response

--- src/schemas/provider.schemas.ts
+++ src/schemas/provider.schemas.ts
@@ -19,7 +19,7 @@ export const CreateProviderBody = Type.Object({
     description: Type.String({ maxLength: 200 }),
     typeService: TypeServices,
     instagram: Type.Optional(Type.String()),
-    emailBusiness: Type.String({format:"email"}),
+    emailBusiness: Type.String({ format: "email" }),
     phoneBusiness: Type.String({
         pattern: "^\\+?[1-9]\\d{7,14}$",
         minLength: 10,
@@ -27,3 +27,25 @@ export const CreateProviderBody = Type.Object({
     }),
 });
 
+export const ProviderUpdateProfile = Type.Object({
+    companyName: Type.Optional(Type.String({ minLength: 10, maxLength: 200 })),
+    description: Type.Optional(Type.String({ maxLength: 200 })),
+    typeService: Type.Optional(TypeServices),
+    instagram: Type.Optional(Type.String()),
+    emailBusiness: Type.Optional(Type.String({ format: "email" })),
+    phoneBusiness: Type.Optional(Type.String({
+        pattern: "^\\+?[1-9]\\d{7,14}$",
+        minLength: 10,
+        maxLength: 11,
+    }))
+});
+
+export const ProviderResponseBody = Type.Object({
+    companyName: Type.String(),
+    description: Type.String(),
+    typeService: Type.String(),
+    instagram: Type.Optional(Type.String()),
+    emailBusiness: Type.String(),
+    phoneBusiness: Type.String(),
+    updatedAt: Type.String({ format: "date-time" }),
+})
