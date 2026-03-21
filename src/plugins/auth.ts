78173bf999950289ff75c133c6edd7fb3208071a
LipehSantos213 <fl9558125@gmail.com>
2026-03-21 10:24:25 -0300

    perf(auth): optimize error

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
 import { prisma } from "../database/prisma";
 import fp from "fastify-plugin";
+import { NotFoundError, UnauthorizedError, UnprocessableEntityError } from "../errors/http.errors";
 
 
 
@@ -11,17 +12,26 @@ export async function authPlugin(app: FastifyInstance) {
     app.decorate(
         "getCurrentUser",
         async (req: FastifyRequest, reply: any) => {
+            // Pega o token no Header e organiza como o payload
             await req.jwtVerify();
 
+            // user.sub pego pelo jwtVerify
             const userId = req.user.sub;
 
+            // Busca o usuario pelo seu ID
             const user = await prisma.usuario.findUnique({
-                where: { id: Number(userId) }
+                where: { id: Number(userId) },
+                include: {
+                    prestador: true
+                }
             });
+
+            // Se não encontrar, despara um error
             if (!user) {
-                throw new Error();
+                throw new NotFoundError("Usuario não encontrado !");
             }
 
+            // Atribui a propriedade da request à os dados do usuario
             req.currentUser = user;
         }
     );
@@ -31,14 +41,14 @@ export async function authPlugin(app: FastifyInstance) {
         (role: string) => {
             return async (req: FastifyRequest, reply: any) => {
                 if (!req.currentUser) {
-                    throw new Error();
+                    throw new UnprocessableEntityError("Não foi possivel buscar Usuario pelo Header");
                 }
                 const userRole = req.currentUser.role;
                 if (role === userRole) {
                     return; // Acesso permitido
                 }
                 else {
-                    throw new Error();
+                    throw new UnauthorizedError("Recurso não autorizado !");
                 }
 
             }
