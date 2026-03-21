b154bbfe07d5491c13c21a9452bc9d215d337d8e
LipehSantos213 <fl9558125@gmail.com>
2026-03-21 10:59:52 -0300

    refactor: organize

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Type, Static } from "@sinclair/typebox";
-import { CreateProviderBody } from "./provider.schemas";
+import { CreateProviderBody, ProviderResponseBody, ProviderUpdateProfile } from "./provider.schemas";
 
 
 
@@ -18,8 +18,24 @@ export const UserCreateBody = Type.Object({
 export type UserCreateType = Static<typeof UserCreateBody>;
 
 
+export const UserUpdateProfileBody = Type.Object({
+    photo: Type.Optional(Type.String()),
+    name: Type.Optional(Type.String()),
+    email: Type.Optional(Type.String({ format: "email" })),
+    phone: Type.Optional(Type.String({
+        pattern: "^\\+?[1-9]\\d{7,14}$",
+        minLength: 10,
+        maxLength: 11,
+    })),
+    provider: Type.Optional(ProviderUpdateProfile)
+});
 
+export type UserUpdateProfileType = Static<typeof UserUpdateProfileBody>;
 
+export const UserUpdatePassword = Type.Object({
+    currentPassword: Type.String(),
+    newPassword: Type.String({ minLength: 8 }),
+})
 
 export const UserRole = Type.Union([
     Type.Literal("CUSTOMER"), // Cliente
@@ -28,10 +44,13 @@ export const UserRole = Type.Union([
 ])
 
 export const UserResponse = Type.Object({
+    id: Type.Integer(),
     name: Type.String(),
     email: Type.String(),
     role: UserRole,
     createdAt: Type.String({ format: "date-time" }),
+    updatedAt: Type.String({ format: "date-time" }),
+    provider: Type.Optional(ProviderResponseBody)
 });
 
 export type UserResponseType = Static<typeof UserResponse>;
