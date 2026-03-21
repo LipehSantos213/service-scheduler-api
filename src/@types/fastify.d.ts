acf8024372e18caefeb3756d07aca84442bc369a
LipehSantos213 <fl9558125@gmail.com>
2026-03-21 11:07:11 -0300

    perf: organize module

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -7,6 +7,7 @@ declare module "fastify" {
     interface FastifyRequest {
         currentUser: { prestador: Prestador | null } & Usuario | null;
     }
+    
     interface FastifyInstance {
         getCurrentUser: (
             req: FastifyRequest,
